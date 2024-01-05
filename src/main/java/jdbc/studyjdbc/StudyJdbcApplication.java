f92ff1b8b92f3bc5ac5b48408d3c7c10530315b3
yunseung <yseungun.21@gmail.com>
2024-01-05 22:20:05 +0900

    feat: 환경 세팅

---
+++ src/main/java/jdbc/studyjdbc/StudyJdbcApplication.java
@@ -0,0 +1,13 @@
+package jdbc.studyjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyJdbcApplication.class, args);
+    }
+
+}
